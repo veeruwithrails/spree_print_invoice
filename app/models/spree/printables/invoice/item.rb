1d8e198037937cbc98a78a1553d0728f7cca100a
veeruwithrails <veeruwithrails@gmail.com>
2018-07-11 19:13:06 +0530

    tax type changes

--- app/models/spree/printables/invoice/item.rb
+++ app/models/spree/printables/invoice/item.rb
@@ -2,9 +2,10 @@ module Spree
   class Printables::Invoice::Item
     extend Spree::DisplayMoney
 
-    attr_accessor :sku, :mrp, :name, :net, :options_text, :price, :quantity, :total, :tax, :tax_type
+    attr_accessor :delivery_charge, :sku, :mrp, :name, :net, :options_text, :price,
+                  :quantity, :total, :tax, :tax_code, :tax_rate, :tax_type
 
-    money_methods :mrp, :net, :price, :total, :tax, :tax_type
+    money_methods :delivery_charge, :mrp, :net, :price, :total, :tax
 
     def initialize(args = {})
       args.each do |key, value|
--- app/models/spree/printables/order/invoice_view.rb
+++ app/models/spree/printables/order/invoice_view.rb
@@ -11,9 +11,11 @@ module Spree
                    :shipments
 
     def items
-      tax = all_adjustments.tax
+      tax_type = shipment_state == stock_loc_state ? 'CGST+SGST' : 'IGST'
       printable.line_items.map do |item|
         tax = item.adjustments.eligible.sum(:amount)
+        tax_cat = item.tax_category
+        tax_rate = tax_cat ? tax_cat.tax_rates.sum(:amount) * 100 : 0
         Spree::Printables::Invoice::Item.new(
           sku: item.variant.sku,
           name: item.variant.name,
@@ -24,7 +26,10 @@ module Spree
           total: item.total,
           mrp: item.variant.product.mrp_price,
           tax: tax,
-          tax_type: item.tax_category.tax_code
+          tax_code: tax_cat.try(:tax_code),
+          tax_rate: tax_rate,
+          tax_type: tax_type,
+          delivery_charge: item.delivery_charge
         )
       end
     end
@@ -37,10 +42,18 @@ module Spree
       printable.tax_address.lastname
     end
 
+    def stock_loc_state
+      shipments.first.stock_location.state_id
+    end
+
+    def shipment_state
+      ship_address.state_id
+    end
+
     private
 
     def all_adjustments
-      printable.all_adjustments.eligible
+      printable.adjustments.eligible
     end
   end
 end
